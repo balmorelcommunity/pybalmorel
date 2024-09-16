f9040c6c8c05f65266906f1add65310d42dd7ef6
Mathias157 <mathiasros@gmail.com>
2024-09-16 17:49:11 +0200

    Decided to keep examples folder to be sure not to mess with my current documentation when merging to master

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,14 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 extensions = [
     "myst_parser",  # in order to use markdown
+    "autoapi.extension",  # in order to use markdown
 ]
+
+# search this directory for Python files
+autoapi_dirs = [".."]
+
+# ignore this file when generating API documentation
+autoapi_ignore = ["*/conf.py"]
 
 myst_enable_extensions = [
     "colon_fence",  # ::: can be used instead of ``` for better rendering
