1749c5a02c4667acff75d44c89d26c4bd1406a2e
Mathias Berg Rosendal <77012503+Mathias157@users.noreply.github.com>
2024-09-25 13:36:39 +0200

    Finished check for file before loading inc files (#17)
    * Started work on issue #12 by renaming the output .gdx file to %scenario_input_data.gdx instead of generic _gams_py_gdb0.gdx
    
    * Finished check for file before loading inc files
    
    * Release 0.3.10
    
    * Merge branch 'master' into check_for_loaded_incfiles

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = "pybalmorel"
 copyright = "2024, Mathias Berg Rosendal, Théodore Le Nalinec"
 author = "Mathias Berg Rosendal, Théodore Le Nalinec"
-release = "0.3.9"
+release = "0.3.10"
 
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".testenv", ".testenv/**"]
 
