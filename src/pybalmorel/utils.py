64bb6968f70e89d576962a3ea0f72c1275863445
Mathias157 <mathiasros@gmail.com>
2024-10-02 22:22:53 +0200

    Large improvement to symbol_to_df, so that it gets the columns from the .gdx if you did not input any columns or it could not find pybalmorel standard formats

--- src/pybalmorel/utils.py
+++ src/pybalmorel/utils.py
@@ -4,6 +4,7 @@ Functions
 
 import gams
 import pandas as pd
+from typing import Tuple
 from .formatting import balmorel_symbol_columns, optiflow_symbol_columns
 
 #%% ------------------------------- ###
@@ -12,22 +13,25 @@ from .formatting import balmorel_symbol_columns, optiflow_symbol_columns
 
 ### 1.0 Converting a GDX file to a pandas dataframe
 def symbol_to_df(db: gams.GamsDatabase, symbol: str, 
-                 cols: str = 'None', 
+                 cols: Tuple[list, None] = None, 
                  parameter_or_set: str = 'parameter',
-                 result_type: str = 'balmorel'):
+                 result_type: str = 'balmorel',
+                 print_explanatory_text: bool = False):
     """
     Loads a symbol from a GDX database into a pandas dataframe
 
     Args:
         db (GamsDatabase): The loaded gdx file
-        symbol (string): The wanted symbol in the gdx file
-        cols (list): The columns
-        parameter_or_set (str): Choose either 'parameter' or 'set'
+        symbol (string): The desired symbol in the gdx file
+        cols (list): Your defined columns, will otherwise first try to find pybalmorel default column formats for the symbol or the raw columns from the gdx  
+        parameter_or_set (str): Is it a GAMS parameter or set? Choose either 'parameter' or 'set'
+        result_type (str): Is it a normal MainResults or a Optiflow Mainresults? Choose either 'balmorel' or 'optiflow'
+        print_explanatory_text (bool): Print the text describing the symbol?
     """   
-    if parameter_or_set == 'parameter':
+    if parameter_or_set.lower() == 'parameter':
         df = dict( (tuple(rec.keys), rec.value) for rec in db[symbol] )
         df = pd.DataFrame(df, index=['Value']).T.reset_index() # Convert to dataframe
-    elif parameter_or_set == 'set':
+    elif parameter_or_set.lower() == 'set':
         df = pd.DataFrame([tuple(rec.keys)  for rec in db[symbol] ])
     else:
         print('Choose either parameter or set!')
@@ -41,18 +45,31 @@ def symbol_to_df(db: gams.GamsDatabase, symbol: str,
         mainresult_symbol_columns = optiflow_symbol_columns
 
     #Name columns
-    if cols == 'None':
+    if cols == None and parameter_or_set.lower() == 'parameter':
         try:
             df.columns = mainresult_symbol_columns[symbol] + ['Unit', 'Value']
-        except ValueError:
+        except (ValueError, KeyError):
             try:
                 df.columns = mainresult_symbol_columns[symbol] + ['Value']
             except KeyError:
-                print('Standard column format not found for this symbol')
+                # If no standard format exists, just use columns from GAMS
+                df.columns = db[symbol].domains_as_strings + ['Value']
+    elif cols == None:
+        try:
+            df.columns = mainresult_symbol_columns[symbol]
+        except (ValueError, KeyError):
+            try:
+                df.columns = mainresult_symbol_columns[symbol]
+            except KeyError:
+                # If no standard format exists, just use columns from GAMS
+                df.columns = db[symbol].domains_as_strings
+        
     else:
         df.columns = cols            
     
-
+    if print_explanatory_text:
+        print(db[symbol].text)
+    
     return df 
 
 def read_lines(name, file_path, make_space=True):
