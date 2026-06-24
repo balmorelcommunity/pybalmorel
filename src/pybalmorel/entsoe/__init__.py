a24e2009ab5ffde673fa45316896b61831f8f829
mathias157 <mathiasros@proton.me>
2026-06-24 10:39:12 +0200

    Readability

--- src/pybalmorel/entsoe/__init__.py
+++ src/pybalmorel/entsoe/__init__.py
@@ -35,16 +35,22 @@ def fetch_day_ahead_prices(api_key, start_date, end_date, bidding_zone):
         pd.DataFrame: DataFrame containing the day-ahead prices.
     """
     client = EntsoePandasClient(api_key=api_key)
-    if type(start_date) is not pd.Timestamp:
-        start_date = pd.Timestamp(start_date, tz="Europe/Brussels")
-    if type(end_date) is not pd.Timestamp:
-        end_date = pd.Timestamp(end_date, tz="Europe/Brussels")
+    start_date = date_format(start_date)
+    end_date = date_format(end_date)
     prices = client.query_day_ahead_prices(
         bidding_zone,
         start=start_date,  # pyright: ignore
         end=end_date,  # pyright: ignore
     )
     return prices
+
+
+def date_format(date):
+    if type(date) is not pd.Timestamp:
+        proper_date = pd.Timestamp(date, tz="Europe/Brussels")
+    else:
+        proper_date = date
+    return proper_date
 
 
 def get_full_year(year: int):
@@ -79,59 +85,61 @@ bidding_zone_codes = {
     "BA": "10YBA-JPCC-----D",
 }
 
+bidding_zones = [
+    "IE_SEM",
+    "10YGB----------A",
+    "PT",
+    "ES",
+    "FR",
+    "BE",
+    "NL",
+    "DE_LU",
+    "10YDK-1--------W",
+    "10YDK-2--------M",
+    "10YNO-1--------2",
+    "10YNO-2--------T",
+    "10YNO-3--------J",
+    "10YNO-4--------9",
+    "10Y1001A1001A48H",
+    "10Y1001A1001A47J",
+    "10Y1001A1001A45N",
+    "10Y1001A1001A46L",
+    "10Y1001A1001A47J",
+    "FI",
+    "EE",
+    "LV",
+    "LT",
+    "PL",
+    "CZ",
+    "SK",
+    "HU",
+    "RO",
+    "BG",
+    "GR",
+    "AL",
+    "MK",
+    "XK",
+    "ME",
+    "RS",
+    "10YBA-JPCC-----D",
+    "HR",
+    "SI",
+    "AT",
+    "CH",
+    "10Y1001A1001A73I",
+    "10Y1001A1001A70O",
+    "10Y1001A1001A71M",
+    "10Y1001A1001A788",
+    "10Y1001C--00096J",
+    "10Y1001A1001A75E",
+    "10Y1001A1001A74G",
+]
+
 
 def get_dayahead_prices(year: int, path: str):
     api_key = getpass("API key for ENTSO-E transparency platform: ")
 
-    for bidding_zone in [
-        "IE_SEM",
-        "10YGB----------A",
-        "PT",
-        "ES",
-        "FR",
-        "BE",
-        "NL",
-        "DE_LU",
-        "10YDK-1--------W",
-        "10YDK-2--------M",
-        "10YNO-1--------2",
-        "10YNO-2--------T",
-        "10YNO-3--------J",
-        "10YNO-4--------9",
-        "10Y1001A1001A48H",
-        "10Y1001A1001A47J",
-        "10Y1001A1001A45N",
-        "10Y1001A1001A46L",
-        "10Y1001A1001A47J",
-        "FI",
-        "EE",
-        "LV",
-        "LT",
-        "PL",
-        "CZ",
-        "SK",
-        "HU",
-        "RO",
-        "BG",
-        "GR",
-        "AL",
-        "MK",
-        "XK",
-        "ME",
-        "RS",
-        "10YBA-JPCC-----D",
-        "HR",
-        "SI",
-        "AT",
-        "CH",
-        "10Y1001A1001A73I",
-        "10Y1001A1001A70O",
-        "10Y1001A1001A71M",
-        "10Y1001A1001A788",
-        "10Y1001C--00096J",
-        "10Y1001A1001A75E",
-        "10Y1001A1001A74G",
-    ]:
+    for bidding_zone in bidding_zones:
         p = Path(path)
         start_date, end_date = get_full_year(year)
         try:
